ae8952457151bb273a327537202d6c950d6141de
hoangcv <hoang.cv@zinza.com.vn>
2023-08-02 16:55:37 +0900

    Add template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
     @users = User.order(:name).page params[:page]
